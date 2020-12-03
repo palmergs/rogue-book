75cdd8b9ff048b4633432c354e93a478fa6df865
Galen Palmer <palmergs@gmail.com>
2020-12-03 16:19:07 -0500

    initial hud

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -7,7 +7,7 @@ pub fn map_render(
 ) {
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
-    for y in camera.top_y .. camera.bottom_y {
+    for y in camera.top_y ..= camera.bottom_y {
         for x in camera.left_x .. camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
